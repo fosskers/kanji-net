39053653059af5154ec3606889f4c02e855acf2e
Colin Woodbury <colin@fosskers.ca>
2020-08-23 21:09:01 -0700

    Don't use SQLite after all
    It doesn't have list support, which I need.

--- kin/src/main.rs
+++ kin/src/main.rs
@@ -1,20 +1,18 @@
 use gumdrop::Options;
 use kn_core::Entry;
-use rusqlite::{params, Connection};
 
 #[derive(Options)]
 struct Args {
     /// Show this help message.
     help: bool,
 
-    /// Path to the SQLite database.
+    /// Path to the Kanji data file.
     #[options(free, required)]
-    db_path: String,
+    data_path: String,
 }
 
 fn main() -> anyhow::Result<()> {
     let args = Args::parse_args_default_or_exit();
-    let c = Connection::open(args.db_path)?;
 
     Ok(())
 }
--- kn-core/src/lib.rs
+++ kn-core/src/lib.rs
@@ -1,29 +1,10 @@
 //! Core types and functions for KanjiNet.
 
-use rusqlite::types::{FromSql, FromSqlError, FromSqlResult, ValueRef};
-use std::str;
-
 /// Wrapper around a library type so that we can give it additional trait
 /// implementations.
 pub struct Kanji(pub kanji::Kanji);
 
-impl FromSql for Kanji {
-    fn column_result(value: ValueRef<'_>) -> FromSqlResult<Self> {
-        match value {
-            ValueRef::Text(bytes) => from_bytes(bytes).ok_or(FromSqlError::InvalidType),
-            _ => Err(FromSqlError::InvalidType),
-        }
-    }
-}
-
-fn from_bytes(bytes: &[u8]) -> Option<Kanji> {
-    str::from_utf8(bytes)
-        .ok()?
-        .chars()
-        .next()
-        .and_then(kanji::Kanji::new)
-        .map(Kanji)
-}
+// TODO Give `Kanji` serde instances via a feature flag.
 
 /// An entry in the kanji database.
 pub struct Entry {
