21290a0bc9d5eab5df22419db10e5217b5ff5dea
Colin Woodbury <colin@fosskers.ca>
2020-08-23 20:31:17 -0700

    [kin] Basic CLI args

--- kin/src/main.rs
+++ kin/src/main.rs
@@ -1,6 +1,20 @@
+use gumdrop::Options;
 use kn_core::Entry;
-use rusqlite::params;
+use rusqlite::{params, Connection};
 
-fn main() {
-    println!("Hello, world!");
+#[derive(Options)]
+struct Args {
+    /// Show this help message.
+    help: bool,
+
+    /// Path to the SQLite database.
+    #[options(free, required)]
+    db_path: String,
+}
+
+fn main() -> anyhow::Result<()> {
+    let args = Args::parse_args_default_or_exit();
+    let c = Connection::open(args.db_path)?;
+
+    Ok(())
 }
