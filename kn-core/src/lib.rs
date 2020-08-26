76fbd956665a1389d6ca47f6a449cb88a391233c
Colin Woodbury <colin@fosskers.ca>
2020-08-26 13:11:21 -0700

    [core] DB IO

--- kn-core/src/lib.rs
+++ kn-core/src/lib.rs
@@ -2,6 +2,9 @@
 
 use kanji::Kanji;
 use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+use std::fs::{self, OpenOptions};
+use std::path::Path;
 
 /// An entry in the kanji database.
 #[derive(Serialize, Deserialize)]
@@ -10,4 +13,30 @@ pub struct Entry {
     pub oya: Vec<Kanji>,
     pub onyomi: Vec<String>,
     pub imi: Vec<(String, String)>,
+}
+
+/// Open a data file and bring the whole "database" into memory.
+pub fn open_db(path: &Path) -> Option<HashMap<Kanji, Entry>> {
+    let raw = fs::read_to_string(path).ok()?;
+    let ks: Vec<Entry> = serde_json::from_str(&raw).ok()?;
+    let mut hm = HashMap::new();
+
+    ks.into_iter().for_each(|e| {
+        hm.insert(e.kanji, e);
+    });
+
+    Some(hm)
+}
+
+pub fn write_db(path: &Path, mut hm: HashMap<Kanji, Entry>) -> Option<()> {
+    let file = OpenOptions::new()
+        .write(true)
+        .create(true)
+        .open(path)
+        .ok()?;
+    let mut entries = hm.drain().map(|(_, v)| v).collect::<Vec<Entry>>();
+    entries.sort_by_key(|e| e.kanji);
+    serde_json::to_writer_pretty(file, &entries).ok()?;
+
+    Some(())
 }
