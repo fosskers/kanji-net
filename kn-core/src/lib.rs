20f4f4b17e6fa56226289d2f2a17a9da844401b0
Colin Woodbury <colin@fosskers.ca>
2020-08-26 13:17:21 -0700

    [core] Use `Result` instead of `Option`

--- kn-core/src/lib.rs
+++ kn-core/src/lib.rs
@@ -6,6 +6,11 @@ use std::collections::HashMap;
 use std::fs::{self, OpenOptions};
 use std::path::Path;
 
+pub enum Error {
+    IO(std::io::Error),
+    JSON(serde_json::Error),
+}
+
 /// An entry in the kanji database.
 #[derive(Serialize, Deserialize)]
 pub struct Entry {
@@ -16,27 +21,25 @@ pub struct Entry {
 }
 
 /// Open a data file and bring the whole "database" into memory.
-pub fn open_db(path: &Path) -> Option<HashMap<Kanji, Entry>> {
-    let raw = fs::read_to_string(path).ok()?;
-    let ks: Vec<Entry> = serde_json::from_str(&raw).ok()?;
+pub fn open_db(path: &Path) -> Result<HashMap<Kanji, Entry>, Error> {
+    let raw = fs::read_to_string(path).map_err(Error::IO)?;
+    let ks: Vec<Entry> = serde_json::from_str(&raw).map_err(Error::JSON)?;
     let mut hm = HashMap::new();
 
     ks.into_iter().for_each(|e| {
         hm.insert(e.kanji, e);
     });
 
-    Some(hm)
+    Ok(hm)
 }
 
-pub fn write_db(path: &Path, mut hm: HashMap<Kanji, Entry>) -> Option<()> {
+pub fn write_db(path: &Path, mut hm: HashMap<Kanji, Entry>) -> Result<(), Error> {
     let file = OpenOptions::new()
         .write(true)
         .create(true)
         .open(path)
-        .ok()?;
+        .map_err(Error::IO)?;
     let mut entries = hm.drain().map(|(_, v)| v).collect::<Vec<Entry>>();
     entries.sort_by_key(|e| e.kanji);
-    serde_json::to_writer_pretty(file, &entries).ok()?;
-
-    Some(())
+    serde_json::to_writer_pretty(file, &entries).map_err(Error::JSON)
 }
