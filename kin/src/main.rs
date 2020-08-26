1fbbcb378167212693cea5efd7486fe6c3cdef81
Colin Woodbury <colin@fosskers.ca>
2020-08-26 12:33:54 -0700

    [kin] `--version` flag

--- kin/src/main.rs
+++ kin/src/main.rs
@@ -1,4 +1,4 @@
-use gumdrop::Options;
+use gumdrop::{Options, ParsingStyle};
 use kn_core::Entry;
 
 #[derive(Options)]
@@ -6,13 +6,21 @@ struct Args {
     /// Show this help message.
     help: bool,
 
+    /// Show the current version of `kin`.
+    version: bool,
+
     /// Path to the Kanji data file.
     #[options(free, required)]
     data_path: String,
 }
 
 fn main() -> anyhow::Result<()> {
-    let args = Args::parse_args_default_or_exit();
+    let args = Args::parse_args_or_exit(ParsingStyle::AllOptions);
+
+    if args.version {
+        let version = env!("CARGO_PKG_VERSION");
+        println!("{}", version);
+    }
 
     Ok(())
 }
